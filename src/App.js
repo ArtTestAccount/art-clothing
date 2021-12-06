c9ddef09e1251bff344eb71f4275c56a0fd38c4d
Arthur Itzhaki <sovietbulldozer@gmail.com>
2021-12-06 15:52:32 -0500

    Implemented email/pw authentication from firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
 import SignInSignOut from './pages/sign-in-sign-out/sign-in-sign-out.component';
 import { Route, Routes } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -19,10 +19,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -37,7 +52,7 @@ class App extends React.Component {
         <Routes>
           <Route exact path='/' element={<Homepage />} />
           <Route exact path='/shop' element={<ShopPage />} />
-          <Route exact path='/sign-in' element={<SignInSignOut />} />
+          <Route exact path='/signin' element={<SignInSignOut />} />
         </Routes>
       </div>
     );
