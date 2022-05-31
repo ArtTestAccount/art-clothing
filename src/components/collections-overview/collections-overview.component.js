7dac688d7fc4f27ee1e21d5379ae5082746b2167
Arthur Itzhaki <sovietbulldozer@gmail.com>
2022-05-31 15:44:10 -0400

    fix: Logo update

--- src/components/collections-overview/collections-overview.component.js
+++ src/components/collections-overview/collections-overview.component.js
@@ -6,6 +6,7 @@ import PreviewCollection from '../preview-collection/preview-collection.componen
 import { selectCollections } from '../../redux/shop/shop.selectors';
 
 const CollectionsOverview = ({ collections }) => {
+  console.log(collections, 'Arthur');
   return (
     <div className='collections-overview'>
       {collections.map(({ id, ...otherCollectionProps }) => (
